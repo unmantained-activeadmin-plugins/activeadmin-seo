c9eaefb00266ea8122e62ffbdfdc7196eb9163a2
Davor Bagarić <davor.bagaric@rebellis.hr>
2014-01-09 16:23:26 +0100

    Update meta.rb for Rails4

--- app/models/active_admin/seo/meta.rb
+++ app/models/active_admin/seo/meta.rb
@@ -1,14 +1,12 @@
 class ActiveAdmin::Seo::Meta < ActiveRecord::Base
   self.table_name = "active_admin_seo_meta"
   belongs_to :seoable, :polymorphic => true
-  attr_accessible :slug, :title, :description, :keywords,
-                  :og_description, :og_site_name, :og_title, :og_type, :og_url
 
   image_accessor :og_image
-  attr_accessible :og_image, :og_image_url, :retained_og_image, :remove_og_image
 
   validates_size_of :og_image, :maximum => 3.megabyte
   validates_property :format, :of => :og_image, :in => [:jpeg, :jpg, :png], :case_sensitive => false
 
   validates :title, :description, :keywords, :length => { :maximum => 160 }
 end
+
